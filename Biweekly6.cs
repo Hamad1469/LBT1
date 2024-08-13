cdf3a701e7781e42d0592fc11e7a1796b8926dce
Hamad1469 <12201469@students.koi.edu.au>
2024-08-13 19:55:31 +1000

    submitting

--- Biweekly6.cs
+++ Biweekly6.cs
@@ -6,7 +6,8 @@ namespace HelloWorldApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I am Hamad Khan");
+            Console.WriteLine("I am 22 years old");
             Console.ReadLine(); 
         }
     }
